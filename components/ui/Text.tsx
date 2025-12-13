9aba0ba584cabc3dd1aa62e1369044c449c1382a
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-12-13 16:40:49 +0600

    final

--- components/ui/Text.tsx
+++ components/ui/Text.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils"
 
- const Title = ({children, className}: {children:React.ReactNoed, className?: string}) => {
+ const Title = ({children, className}: {children:React.ReactNode, className?: string}) => {
     return (
         <h2 className={cn("text-3xl font-bold text-shop_btn_dark_green capitalize tracking-wide font-sans", className)}>{children}</h2>
     )
