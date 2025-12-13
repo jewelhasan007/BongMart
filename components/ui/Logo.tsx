e7a18931dd7e6c6a8cc0e8eaad57b3b7d8894863
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-12-13 16:47:12 +0600

    final

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -9,13 +9,26 @@ interface LogoProps {
     onClick?: () => void;
 }   
         
-const Logo: FC<LogoProps> = ({className, spanDesign, as: Component = 'div', onClick}:{className?: string, spanDesign?: string}) => {
+const Logo: FC<LogoProps> = ({className, spanDesign}:{className?: string, spanDesign?: string}) => {
     return (
-        <Component onClick={onClick} className={className} href={"/"}>
-            <h2 className={cn("text-2xl text-shop_btn_dark_green font-black tracking-wider uppercase hover:text-shop_light_green", className )}> BongMar
-                <span  className={cn("text-shop_light_green group-hover:text-shop_btn_dark_green hoverEffect", spanDesign)}>t</span>
-                </h2>
-        </Component>
+        <Link href={"/"} className="inline-flex">
+      <h2
+        className={cn(
+          "text-2xl text-shop_dark_green font-black tracking-wider uppercase hover:text-shop_light_green hoverEffect group font-sans",
+          className
+        )}
+      >
+        Shopcar
+        <span
+          className={cn(
+            "text-shop_light_green group-hover:text-shop_dark_green hoverEffect",
+            spanDesign
+          )}
+        >
+          t
+        </span>
+      </h2>
+    </Link>
     );
 };
 
--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -16,25 +16,41 @@ const SideMenu : FC<SidebarProps> = ({isOpen, onClose}) => {
     const pathname = useParams();
     const sidebarRef = useOutSideClick<HTMLDivElement>(onClose)
     return (
-        <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/70 shadow-xl ${isOpen ? "translate-x-0" : "-translate-x-full"} hoverEffect`}>
-         
-         <div ref={sidebarRef} className='min-w-72 mas-w-96 bg-black h-screen p-10 border-r-shop_light_green flex flex-col gap-6'>
-            <div className='flex items-center justify-between gap-5'>
-                <Logo className='text-white' spanDesign="group-hover:text-white"></Logo>
-                <button onClick={onClose} className='hover:text-shop_light_green hoverEffect'>
-                    <X></X>
-                </button>
-            </div>
-            <div className='flex flex-col space-y-3.5 font-semibold tracking-wide'>
-                {headerData?.map((item) => (
-                    <Link href={item?.href} key={item?.title} className={`hover:text-shop_light_green hoverEffect ${pathname === item?.href && "text-white"}`}>
-                    {item?.title}
-                    </Link>
-                ))}
-            </div>
-            <SocialMedia></SocialMedia>
-         </div>
+      <div
+      className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/70 shadow-xl ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      } hoverEffect`}
+    >
+      <div
+        ref={sidebarRef}
+        className="min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6"
+      >
+        <div className="flex items-center justify-between gap-5">
+          <Logo className="text-white" spanDesign="group-hover:text-white" />
+          <button
+            onClick={onClose}
+            className="hover:text-shop_light_green hoverEffect"
+          >
+            <X />
+          </button>
         </div>
+
+        <div className="flex flex-col space-y-3.5 font-semibold tracking-wide">
+          {headerData?.map((item) => (
+            <Link
+              href={item?.href}
+              key={item?.title}
+              className={`hover:text-shop_light_green hoverEffect ${
+                pathname === item?.href && "text-white"
+              }`}
+            >
+              {item?.title}
+            </Link>
+          ))}
+        </div>
+        <SocialMedia />
+      </div>
+    </div>
     );
 };
 
