5b60aa39f8844439e4bd73db5034bef2084b2c22
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-12-17 17:36:40 +0600

    final

--- components/ui/Search.tsx
+++ components/ui/Search.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Search = () => {
     return (
-        <div>
+        <div className="w-5 h-5 hover:text-shop_light_green hoverEffect"> 
             Search
         </div>
     );
