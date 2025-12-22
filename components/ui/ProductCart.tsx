c24a3444eeb3cdb771231d1eba69709b759fbe59
Jewel Hasan <jewel.hasan@cg-bd.com>
2025-12-22 16:48:15 +0600

    part4_42.58 min

---
+++ components/ui/ProductCart.tsx
@@ -0,0 +1,16 @@
+import { Product } from '@/sanity.types';
+import { urlFor } from '@/sanity/lib/image';
+import Image from 'next/image';
+import React from 'react';
+
+const ProductCart = ({product}: {product: Product}) => {
+    return (
+        <div>
+            <div>
+                {product?.images && <Image src={urlFor(product?.images[0]).url()} alt='ProductImage' loading='lazy' width={700} height={700}></Image>}
+            </div>
+        </div>
+    );
+};
+
+export default ProductCart;
--- components/ui/ProductGrid.tsx
+++ components/ui/ProductGrid.tsx
@@ -4,11 +4,14 @@ import { useEffect, useState } from "react";
 import HomeTabBar from "./HomeTabBar";
 import { productType } from "@/constants/data";
 import { client } from "@/sanity/lib/client";
-import {motion} from "motion/react"
+import {animate, AnimatePresence, motion} from "motion/react"
 import { LoaderIcon } from "lucide-react";
+import NoProductAvailable from "./NoProductAvailable";
+import ProductCart from "./ProductCart";
+import { Product } from "@/sanity.types";
 
 const ProductGrid = () => {
-    const [products, setProducts] = useState([])
+    const [products, setProducts] = useState<Product[]>([])
     const [loading, setLoading] = useState(false)
     const [selectedTab, setSelectedTab] = useState(productType[0]?.title || "")
 
@@ -30,11 +33,24 @@ const ProductGrid = () => {
     return (
         <div>
        <HomeTabBar selectedTab={selectedTab} onTabSelect={setSelectedTab}></HomeTabBar>
-       {!loading ? <div className="fles flex-col items-center justify-center py-10 min-h-80 gap-4 bg-gray-100 w-full mt-10">
-        <div className="space-x-2 flex items-center text-blue-600"><LoaderIcon className="w-5 h-6 animate-spin"></LoaderIcon> <span>Product is laoding...</span>
+       {loading ? (<div className="fles flex-col items-center justify-center py-10 min-h-80 gap-4 bg-gray-100 w-full mt-10">
+        <div className="space-x-2 flex justify-center text-blue-600"><LoaderIcon className="w-5 h-6 animate-spin"></LoaderIcon> <span>Product is laoding...</span>
         
         </div >
-       </div> : <>products</> }
+       </div>) : products?.length? (
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-2.5 mt-10">{
+    products?.map((product) =>(
+        <AnimatePresence key={product?._id}>
+            <motion.div 
+            layout 
+            initial={{opacity: 0.2}} 
+            animate={{opacity:1}} 
+            exit={{opacity:0}}>
+                <ProductCart product={product}></ProductCart>
+            </motion.div>
+        </AnimatePresence>
+    ))}
+    </div>):(<NoProductAvailable></NoProductAvailable>) }
         </div>
     );
 };
