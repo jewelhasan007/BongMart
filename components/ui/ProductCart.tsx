41d43b88baa897eb4e66bd7051182168a8fe1a8b
Jewel Hasan <jewel.hasan@cg-bd.com>
2026-01-01 14:26:39 +0600

    P4_51:24

--- components/ui/ProductCart.tsx
+++ components/ui/ProductCart.tsx
@@ -1,13 +1,34 @@
 import { Product } from '@/sanity.types';
 import { urlFor } from '@/sanity/lib/image';
+import { Flame } from 'lucide-react';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const ProductCart = ({product}: {product: Product}) => {
     return (
-        <div>
-            <div>
+        <div className='text-sm border-[1px] border-dark_blue/20 rounded-md bg-white group'>
+            <div className='relative group overflow-hidden bg-shop_light_bg'>
                 {product?.images && <Image src={urlFor(product?.images[0]).url()} alt='ProductImage' loading='lazy' width={700} height={700}></Image>}
+             {product?.status === "sale" ? (
+          <p className="absolute top-2 left-2 z-10 text-xs border border-darkColor/50 px-2 rounded-full group-hover:border-lightGreen hover:text-shop_dark_green hoverEffect">
+            Sale!
+          </p>
+        ) : (
+          <Link
+            href={"/deal"}
+            className="absolute top-2 left-2 z-10 border border-shop_orange/50 p-1 rounded-full group-hover:border-shop_orange hover:text-shop_dark_green hoverEffect"
+          >
+            <Flame
+              size={18}
+              fill="#fb6c08"
+              className="text-shop_orange/50 group-hover:text-shop_orange hoverEffect"
+            />
+          </Link>
+        )}
+            </div>
+            <div className='p-3 flex '>
+                Produt details
             </div>
         </div>
     );
